2745eda77b6905e18f75e9aea66d23b68808c23b
Lucas Herich <github.com@herich.net>
2015-02-01 03:11:19 +0100

    added comments

--- ValidatorDE.php
+++ ValidatorDE.php
@@ -1,15 +1,21 @@
 <?php
+/**
+ * Recapo <http://recapo.de>
+ *
+ * @link      http://github.com/lherich/recapo.de
+ * @copyright Copyright (c) 2014 Lucas Herich <info@recapo.de>
+ * @license   MIT License <http://recapo.de/license>
+ */
 
 namespace Valitron;
 
-use InvalidArgumentException;
-
+/**
+ *
+ */
 class ValidatorDE extends \Valitron\Validator
 {
-  public function __construct($data, $fields = array(), $lang = null, $langDir = null) {
-
-    return parent::__construct($data, $fields, 'de', 'Valitron/lang');
-  }
-  
+    public function __construct($data, $fields = array(), $lang = null, $langDir = null)
+    {
+        return parent::__construct($data, $fields, 'de', 'Valitron/lang');
+    }
 }
-?>
